c7d6ccabf7e50ba42413fef3ad1191e9026f8ed1
JACKT72xp <48709035+JACKT72xp@users.noreply.github.com>
2020-04-05 22:19:16 +0200

    Update main.tf

--- aws/certificate-manager/main.tf
+++ aws/certificate-manager/main.tf
@@ -28,7 +28,6 @@ resource "aws_route53_record" "cert_validation" {
 }
 
 resource "aws_acm_certificate_validation" "default" {
-  provider = "aws.acm"
   certificate_arn = "${aws_acm_certificate.cert.arn}"
   validation_record_fqdns = [
     "${aws_route53_record.validation.fqdn}",
