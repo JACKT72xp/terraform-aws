14ec80a5e092aa48acbaedd3288804b9c8526b0f
JACKT72xp <48709035+JACKT72xp@users.noreply.github.com>
2020-04-06 06:11:35 +0200

    fixed 2

--- aws/elastic-load-balancer/main.tf
+++ aws/elastic-load-balancer/main.tf
@@ -8,7 +8,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "subnet_ect" {
-  count             = var.az_count
+  #count             = var.az_count
   cidr_block        = "${cidrsubnet(aws_vpc.main.cidr_block, 8, count.index)}"
   availability_zone = "${data.aws_availability_zones.available.names[count.index]}"
   vpc_id            = "${aws_vpc.main.id}"
